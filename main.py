746a4b00f242cecd4ea0c7711b0e12abb067bb74
Cisco89 <franciscoadriangarcia1@gmail.com>
2019-07-10 22:26:43 -0600

    Completed assignment requirements

--- main.py
+++ main.py
@@ -5,16 +5,32 @@ import os
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+# checking that password and validate password match each other
 def validate_passwords(value_1, value_2):
     if value_1 == value_2:
         return True
 
     return False
 
+# return error if there is a space of if length is invalid
 def correct_entry(value):
-    if 3 < len(value) and 21 > len(value):
+    if ' ' in value:
+        return False
+    if 3 <= len(value) and 20 >= len(value):
         return True
     
+    return False
+
+# return error if there is space, invalid length, or missing characters "@" and "."
+def correct_email_entry(email_entry):
+    # checking if field was left blank(which is okay)
+    if email_entry == '':
+        return True
+    # checking for special characters "@", "." 
+    # and validating correct length by calling previouly created function 
+    if '@' in email_entry and '.' in email_entry and correct_entry(email_entry):
+        return True
+
     return False
 
 @app.route("/")
@@ -26,6 +42,7 @@ def index():
 
 @app.route("/", methods=['POST'])
 def welcome():
+    # providing values from the form data sent on POST method
     username = request.form['user_name']
     password = request.form['password']
     validate_password = request.form['validate_password']
@@ -41,12 +58,17 @@ def welcome():
         validate_password_error = 'Passwords must match!'
 
     if not correct_entry(username):
-        username_error = "Username must consist of 3-20 characters!"
+        username_error = "Username cannot contain spaces and must consist of 3-20 characters!"
 
     if not correct_entry(password):
-        password_error = "Password must consist of 3-20 characters!"
+        password_error = "Password cannot contain spaces and must consist of 3-20 characters!"
 
-    if not validate_password_error and not username_error and not password_error:
+    if not correct_email_entry(email):
+        email_error = "Email cannot contain spaces and must consist of 3-20 characters and follow the format of an email address ex. someone@gmail.com"
+
+    # checking there are no errors occuring
+    # if error occurs stay in signup page, but keep username and display errors
+    if not validate_password_error and not username_error and not password_error and not email_error:
         return render_template(
             'welcome.html', 
             title='Welcome', 
@@ -60,6 +82,7 @@ def welcome():
             username_error=username_error,
             password_error=password_error,
             validate_password_error=validate_password_error,
+            email_error=email_error,
             )
 
 app.run()
